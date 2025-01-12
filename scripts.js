6b48e4267cc9b3d98f52213f79872fb9e9f8adc1
Ahmed Mustafa <107986169+ahmedmustafam@users.noreply.github.com>
2025-01-12 17:13:36 +0400

    Refactor code to fix selection bug
    Result was incorrectly displaying (eg scissors
    beats rock) and overall code was not very readable
    . Code was reorganized logic was rewritten.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
 
-// Declaring variables to be html elements
+// Declaring DOM Elements
+
 const rockButton = document.createElement("button");
 rockButton.setAttribute("id", "rock");
 rockButton.textContent = "Rock";
@@ -34,7 +32,8 @@ container.appendChild(scoreCount);
 
 document.body.appendChild(container);
 
-// Event Listener function goes here
+// Event Listener Function
+
 let selection;
 
 const buttons = document.querySelectorAll("button");
@@ -46,96 +45,80 @@ buttons.forEach(button => {
             selection = "Paper";
         } else if (button.id === "scissors") {
             selection = "Scissors";
-        }
+        } 
     });
 });
 
-
-function getComputerChoice() {
-    let choice = getRandomInt(3);
-    let play = (choice === 0) ? "Rock" :
-        (choice === 1) ? "Paper" :
-        "Scissors";
-    return play;
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
 }
-
 
 function getHumanChoice() {
-    return selection.charAt(0).toUpperCase() + selection.slice(1).toLowerCase();
+    return selection;
 }
 
+let choice; 
 
-// Logic to play game
+function getComputerChoice() {
+    let randomNumber = getRandomInt(3);
+    choice = (randomNumber === 0) ? "Rock" :
+        (randomNumber === 1) ? "Paper" :
+        "Scissors";
+    return choice;
+}
+
+// functions to play game 
+
 let humanScore = 0;
 let computerScore = 0;
-let roundPlayed = 0;
+let roundsPlayed = 0;
 
-function playGame() {
-    
-    function resetGame() {
-        alert(`Game Over! Final Score: You: ${humanScore}, Computer: ${computerScore}` );
-        scoreResult.textContent = "Game Over!, Final Score:";
-        selectionResult.textContent = "";
-        console.log("Game reset");
-        humanScore = 0;
-        computerScore = 0;
-        roundPlayed = 0;
-        
-        
-    }
+function playRound(humanChoice, computerChoice) {
+    ++roundsPlayed;
+    selectionResult.textContent = `You Chose ${humanChoice}, Computer chose ${computerChoice}`;
 
-    function playRound(humanChoice, computerChoice) {
-        ++roundPlayed;
-        let hum = humanChoice; // Script was incorrectly displaying winner before this was added.
-        let comp = computerChoice; // Temporary values to store the random value so that winner can be determined correctly.
-        selectionResult.textContent = `You Chose ${hum}, Computer chose ${comp}`;
-        console.log(`You Chose ${hum}`);
-        console.log(`Computer Chose ${comp}`);
-
-        if (hum === "Rock" && comp === "Scissors" ||
-            hum === "Paper" && comp === "Rock" ||
-            hum === "Scissors" && comp === "Paper"
-        ) {
-            console.log("You Win!");
-            ++humanScore;
-            console.log(`Score: You: ${humanScore}, Computer: ${computerScore}`);
-        } else if (hum === "Rock" && comp === "Paper" ||
-            hum === "Paper" && comp === "Scissors" ||
-            hum === "Scissors" && comp === "Rock"
-         ) {
-            console.log("You Lose.");
-            ++computerScore;
-            console.log(`Score: You: ${humanScore}, Computer: ${computerScore}`);
-         } else {
-            console.log("Draw")
-         }
-         
-    }
-
-    console.log(`Final Score: ${humanScore}, Compter: ${computerScore}`);
-    scoreCount.textContent = `Score: You: ${humanScore}, Computer: ${computerScore}`;
-    let score1 = humanScore;
-    let score2 = computerScore;
-
-    if (score1 > score2) {
-        scoreResult.textContent = "Congratulations, you won!";
-        console.log("Congratulations, you won!");
-    } else if (score2 > score1) {
-        scoreResult.textContent = "Better luck next time!";
-        console.log("Better luck next time!");
+    if (humanChoice === "Rock" && computerChoice === "Scissors" ||
+        humanChoice === "Paper" && computerChoice === "Rock" ||
+        humanChoice === "Scissors" && computerChoice === "Paper"
+    ) {
+        ++humanScore;
+        console.log("You Win");
+        scoreResult.textContent = `Congratulations, you win.`
+    } else if (humanChoice === "Rock" && computerChoice === "Paper" ||
+        humanChoice === "Paper" && computerChoice === "Scissors" ||
+        humanChoice === "Scissors" && computerChoice === "Rock"
+    ) {
+        ++computerScore;
+        console.log("You Lose");
+        scoreResult.textContent = `You Lose. Try again.`;
     } else {
         scoreResult.textContent = "Draw";
-        console.log("Draw")
+        console.log("Draw");
     }
+  }
 
-    //statement to reset after 5 rounds
-    if (roundPlayed === 5) {
+
+function resetGame() {
+    alert(`Gamer Over! Final Score: You: ${humanScore}, Computer: ${computerScore}`);
+    scoreResult.textContent = `Game Over! Final Score:`;
+    selectionResult.textContent = '';
+    console.log("Game Reset");
+    humanScore = 0;
+    computerScore = 0;
+    roundsPlayed = 0;
+}
+
+function playGame() {
+    playRound(getHumanChoice(), getComputerChoice());
+    scoreCount.textContent = `Score: You: ${humanScore}, Computer: ${computerScore}`;
+    
+    if (roundsPlayed === 5) {
         resetGame();
     }
-    
-    playRound(getHumanChoice(), getComputerChoice());
 }
+
+//starting the game with click
 
 buttons.forEach(button => {
     button.addEventListener("click", playGame);
-});
+})
